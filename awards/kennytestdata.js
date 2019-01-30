1f90e9f4c40a7f474d32fe6d5d684feeee1b5a1f
openbirdstudio <47178989+openbirdstudio@users.noreply.github.com>
2019-01-30 20:13:29 +0800

    Add files via upload

---
+++ awards/kennytestdata.js
@@ -0,0 +1,191 @@
+var storymap_data = {
+    "storymap": {
+        "slides": [{
+            "type": "overview",
+            "date": "1790-2010",
+            "text": {
+                "headline": "Awards <small>openbird studio</small>",
+                "text": "<p> This part is shown based on the technique called <b> storymap </b>. You will see what we have achieved all over the world. </p>" +
+                        "<div>" +
+                          '<table class="overviwTable">' +
+                            "<tbody>" +
+                                "<th>Year</th>" +
+                                "<th>Event</th>" +
+                                "<th>Prize</th>" +
+                                
+                                "<tr>" +
+                                  "<td> 2015 </td>" +
+                                  "<td> MIT future city workshop </td>" +
+                                  "<td> 1st Prize </td>" +
+                                "</tr>" +
+
+                                "<tr>" +
+                                  "<td> 2016 </td>" +
+                                  "<td> Invited to MIT Media Lab </td>" +
+                                  "<td> - </td>" +
+                                "</tr>" +
+
+                                "<tr>" +
+                                  "<td> 2017 </td>" +
+                                  "<td> Hsinchu Hackathon </td>" +
+                                  "<td> 1st Prize </td>" +
+                                "</tr>" +
+
+                                "<tr>" +
+                                  "<td> 2018 </td>" +
+                                  "<td> Data Application Innovation </td>" +
+                                  "<td> 3rd Prize </td>" +
+                                "</tr>" +
+
+                                "<tr>" +
+                                  "<td> 2018 </td>" +
+                                  "<td> 104 Hackathon </td>" +
+                                  "<td> 1st Prize </td>" +
+                                "</tr>" +
+
+                                "<tr>" +
+                                  "<td> 2018 </td>" +
+                                  "<td> Asia Hackathon </td>" +
+                                  "<td> best apllication </td>" +
+                                "</tr>" +
+
+                                "<tr>" +
+                                  "<td> 2018 </td>" +
+                                  "<td> Invited to Seoul, Korea </td>" +
+                                  "<td> - </td>" +
+                                "</tr>" +
+
+                            "</tbody>" +
+                          "</table>" + 
+                        "</div>"
+            }
+        },{
+            "date": "1790-2010",
+            "text": {
+                "headline": "MIT future city workshop <small>2015</small>",
+                "text": "<p>serendipity</p>"
+            },
+            "location":{
+                "name" : "Kyoto",
+                "lat":25.040498,
+                "lon":121.539666,
+                "zoom":10,
+                "line":true
+            },
+            "media": {
+                    "url":"",
+                    "credit":"資料來源(credit)",
+                    "caption":"圖表標題"
+                }
+        },{
+            "date": "1790-2010",
+            "text": {
+                "headline": "Invited to MIT Media Lab<small>2016</small>",
+                "text": "<p>serendipity</p>"
+            },
+            "location":{
+                "name" : "Kyoto",
+                "lat":42.360393,
+                "lon":-71.087356,
+                "zoom":10,
+                "line":true
+            },
+            "media": {
+                    "url":"",
+                    "credit":"資料來源(credit)",
+                    "caption":"圖表標題"
+                }
+        },{
+            "date": "1790-2010",
+            "text": {
+                "headline": "Hsinchu Hackathon <small>2017</small>",
+                "text": '<img src="../projects/hsinchuHackthon/hsinchuHack1.jpg">'+
+                        '<p>We developed a webGIS for business analysis using location intelligence.</p>'
+            },
+			"location":{
+				"name" : "Kyoto",
+				"lat":24.799496,
+				"lon":120.979003,
+				"zoom":10,
+				"line":true
+			},
+            "media": {
+                    "url":"",
+                    "credit":"資料來源(credit)",
+                    "caption":"圖表標題"
+                }
+        },{
+            "date": "1790-2010",
+            "text": {
+                "headline": "Data Application Innovation <small>2018</small>",
+                "text": "<p></p>"
+            },
+            "location":{
+                "name" : "Kyoto",
+                "lat":25.077700,
+                "lon":121.524274,
+                "zoom":10,
+                "line":true
+            },
+            "media": {
+                    "url":"",
+                    "credit":"資料來源(credit)",
+                    "caption":"圖表標題"
+                }
+        },{
+            "date": "1790-2010",
+            "text": {
+                "headline": "104 Hackthon <small>2018</small>",
+                "text": "<p></p>"
+            },
+            "location":{
+                "name" : "Kyoto",
+                "lat":25.045460,
+                "lon":121.531093,
+                "zoom":10,
+                "line":true
+            },
+            "media": {
+                    "url":"",
+                    "credit":"資料來源(credit)",
+                    "caption":"圖表標題"
+                }
+        },{
+            "date": "1790-2010",
+            "text": {
+                "headline": "Asia Hackthon <small>2018</small>",
+                "text": '<p>jkjks</p>'
+            },
+            "location":{
+                "name" : "Kyoto",
+                "lat":25.048913,
+                "lon":121.561812,
+                "zoom":10,
+                "line":true
+            },
+            "media": {
+                    "url":"",
+                    "credit":"資料來源(credit)",
+                    "caption":"圖表標題"
+                }
+        },{
+            "date": "1790-2010",
+            "text": {
+                "headline": "Invited to Seoul, Korea <small>2018</small>",
+                "text": "<p></p>"
+            },
+            "location":{
+                "name" : "Kyoto",
+                "lat":37.568206,
+                "lon":126.976495,
+                "zoom":10,
+                "line":true
+            },
+            "media": {
+                    "url":"",
+                    "credit":"資料來源(credit)",
+                    "caption":"圖表標題"
+                }
+        }]
+    }
+};
